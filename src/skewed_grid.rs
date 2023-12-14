5ab9249dadf2949ab4f6b70af0216d3861129dee
Rasmus Brönnegård <1162652+rasmusgo@users.noreply.github.com>
2023-12-14 22:00:32 +0100

    Skewed grid LOD

--- src/skewed_grid.rs
+++ src/skewed_grid.rs
@@ -1,43 +1,15 @@
-//! Demonstrates the most barebone usage of the Rerun SDK.
-
 use rerun::external::glam;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let rec = rerun::RecordingStreamBuilder::new("skewed grid").spawn()?;
 
-    // Skew the points so that the circles are stacked
+    // Skew the points so that the spheres are packed in a hexagonal pattern.
     let cos_30 = 30.0_f32.to_radians().cos();
     let mut z_axis = glam::vec3(0.5, cos_30 / 3.0, 0.0);
     z_axis.z = (1.0 - z_axis.x * z_axis.x - z_axis.y * z_axis.y).sqrt();
     let world_from_grid =
         glam::Mat3::from_cols_array_2d(&[[1.0, 0.0, 0.0], [0.5, cos_30, 0.0], z_axis.into()]);
-    let mut points = Vec::new();
-    let n = 100;
-    let positive_sphere_center =
-        world_from_grid * glam::vec3(0.5 * n as f32, 0.5 * n as f32, 0.5 * n as f32);
-    let positive_sphere_radius = 0.3 * n as f32;
-    let negative_sphere_center =
-        world_from_grid * glam::vec3(0.25 * n as f32, 0.5 * n as f32, 0.5 * n as f32);
-    let negative_sphere_radius = 0.2 * n as f32;
-    for i in 0..n {
-        for j in 0..n {
-            for k in 0..n {
-                let p_in_grid = glam::vec3(i as f32, j as f32, k as f32);
-                let p = world_from_grid * p_in_grid;
-                if (p - negative_sphere_center).length() < negative_sphere_radius {
-                    continue;
-                }
-                if (p - positive_sphere_center).length() < positive_sphere_radius {
-                    points.push(p);
-                }
-            }
-        }
-    }
-    rec.log(
-        "my_points",
-        &rerun::Points3D::new(points.clone()).with_radii([0.6]),
-    )?;
-    rec.log(
+    rec.log_timeless(
         "axes",
         &rerun::Arrows3D::from_vectors([
             world_from_grid.x_axis,
@@ -51,38 +23,69 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         ])
         .with_origins([glam::vec3(0.0, 0.0, 0.0); 3]),
     )?;
-    rec.log(
+    rec.log_timeless(
         "bounds",
         &rerun::Arrows3D::from_vectors([
-            world_from_grid.x_axis * n as f32,
-            world_from_grid.x_axis * n as f32,
-            world_from_grid.x_axis * n as f32,
-            world_from_grid.x_axis * n as f32,
-            world_from_grid.y_axis * n as f32,
-            world_from_grid.y_axis * n as f32,
-            world_from_grid.y_axis * n as f32,
-            world_from_grid.y_axis * n as f32,
-            world_from_grid.z_axis * n as f32,
-            world_from_grid.z_axis * n as f32,
-            world_from_grid.z_axis * n as f32,
-            world_from_grid.z_axis * n as f32,
+            world_from_grid.x_axis,
+            world_from_grid.x_axis,
+            world_from_grid.x_axis,
+            world_from_grid.x_axis,
+            world_from_grid.y_axis,
+            world_from_grid.y_axis,
+            world_from_grid.y_axis,
+            world_from_grid.y_axis,
+            world_from_grid.z_axis,
+            world_from_grid.z_axis,
+            world_from_grid.z_axis,
+            world_from_grid.z_axis,
         ])
         .with_colors([rerun::Color::from_u32(0xaaaaaaff)])
         .with_origins([
             glam::vec3(0.0, 0.0, 0.0),
-            world_from_grid.y_axis * n as f32,
-            world_from_grid.z_axis * n as f32,
-            world_from_grid.y_axis * n as f32 + world_from_grid.z_axis * n as f32,
+            world_from_grid.y_axis,
+            world_from_grid.z_axis,
+            world_from_grid.y_axis + world_from_grid.z_axis,
             glam::vec3(0.0, 0.0, 0.0),
-            world_from_grid.x_axis * n as f32,
-            world_from_grid.z_axis * n as f32,
-            world_from_grid.x_axis * n as f32 + world_from_grid.z_axis * n as f32,
+            world_from_grid.x_axis,
+            world_from_grid.z_axis,
+            world_from_grid.x_axis + world_from_grid.z_axis,
             glam::vec3(0.0, 0.0, 0.0),
-            world_from_grid.x_axis * n as f32,
-            world_from_grid.y_axis * n as f32,
-            world_from_grid.x_axis * n as f32 + world_from_grid.y_axis * n as f32,
+            world_from_grid.x_axis,
+            world_from_grid.y_axis,
+            world_from_grid.x_axis + world_from_grid.y_axis,
         ]),
     )?;
+
+    for lod in 2..8 {
+        rec.set_time_sequence("LOD", lod as i64);
+        let n = 1 << lod;
+        let node_radius = 0.6 / n as f32;
+        let mut points = Vec::new();
+        let positive_sphere_center = world_from_grid * glam::vec3(0.5, 0.5, 0.5);
+        let positive_sphere_radius = 0.3;
+        let negative_sphere_center = world_from_grid * glam::vec3(0.25, 0.5, 0.5);
+        let negative_sphere_radius = 0.2;
+        for i in 0..n {
+            for j in 0..n {
+                for k in 0..n {
+                    let p_in_grid = glam::vec3(i as f32, j as f32, k as f32) / n as f32;
+                    let p = world_from_grid * p_in_grid;
+                    if (p - negative_sphere_center).length() + node_radius < negative_sphere_radius
+                    {
+                        continue;
+                    }
+                    if (p - positive_sphere_center).length() + node_radius < positive_sphere_radius
+                    {
+                        points.push(p);
+                    }
+                }
+            }
+        }
+        rec.log(
+            "my_points",
+            &rerun::Points3D::new(points.clone()).with_radii([node_radius]),
+        )?;
+    }
 
     Ok(())
 }
